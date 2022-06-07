dbe53d81c0ccd2a979299dc66a720c3446a2c202
Gouaby Kalivogui <gouabyk14@yahoo.fr>
2022-06-07 20:12:48 +0500

    added ipfs-react-router, mobx, urql

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 1. clone this directory 
 2. run ```npm i```
 3. run ```npm start```
+4. run ```npm run build```
+5. run ```ipfs add -r build```(copy this CID)
+6. run ```ipfs pin remote add --service=Pinata --name=MelalieGraph_3.0.3 (add the CID here)```
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import App from './app/App'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
 import 'perfect-scrollbar/css/perfect-scrollbar.css'
 import * as serviceWorker from './serviceWorker'
 import { StyledEngineProvider } from '@mui/styled-engine'
 import { CssBaseline } from '@mui/material'
+import IpfsRouter from 'ipfs-react-router'
 
 ReactDOM.render(
     <StyledEngineProvider injectFirst>
-        <BrowserRouter>
+        <IpfsRouter>
             <CssBaseline />
             <App />
-        </BrowserRouter>
+        </IpfsRouter>
     </StyledEngineProvider>,
     document.getElementById('root')
 )
